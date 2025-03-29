9dc37307b4e22df22442c26048eef3e2bd421df4
CarlosAssis95 <carlos.e.a@outlook.com>
2025-03-29 18:19:02 -0300

    config finalizado

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,14 +3,27 @@ package config
 import (
 	"Controle-Despesas/internal/infrastructure"
 	"github.com/joho/godotenv"
+	"os"
 )
 
 type Config struct {
-	Database *database.Config
+	Database database.Config
 }
 
 func Load() (*Config, error) {
 	if err := godotenv.Load(); err != nil {
 		return nil, err
 	}
+
+	return &Config{
+		Database: database.Config{
+			User:            os.Getenv("DB_USER"),
+			Host:            os.Getenv("DB_HOST"),
+			Password:        os.Getenv("DB_PASSWORD"),
+			DBName:          os.Getenv("DB_NAME"),
+			Port:            os.Getenv("DB_PORT"),
+			ApplicationName: os.Getenv("APPLICATION_NAME"),
+			SSLMode:         os.Getenv("DB_SSLMODE"),
+		},
+	}, nil
 }
